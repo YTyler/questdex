f140e01a80a72342e158d9511b905a76113a4d2e
YTyler <tyates907@gmail.com>
2021-11-19 14:49:51 -0800

    revert navbar classname

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <div className="Header">
+    <div className="Navbar">
       <h1>QuestDex</h1>
       <h3 className="LoginNav">Login/Register</h3>
     </div>
