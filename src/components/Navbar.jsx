aa4e8bb2e13d4b6cc9f3c76e15b9634b7540d561
Tyler Yates <tyates907@gmail.com>
2021-11-22 15:10:57 -0800

    add page links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import { GiSwapBag, GiSwordSpade } from "react-icons/gi";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <div className="Navbar">
-      <h1>QuestDex</h1>
-      <h3 className="LoginNav">Login/Register</h3>
+      <div className="LeftNav">
+        <h1>QuestDex</h1>
+        <Link to="/login">
+          <h3 className="LoginNav">Login/Register</h3>
+        </Link>
+      </div>
+      <nav className="RightNav">
+        <Link to="quests" className="NavButton">
+          <GiSwordSpade />
+          <h3>Quests</h3>
+        </Link>
+        <Link to="items" className="NavButton">
+          <GiSwapBag />
+          <h3>Items</h3>
+        </Link>
+      </nav>
     </div>
   );
 }
