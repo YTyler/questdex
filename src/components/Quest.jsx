2a95802a439d9788cdb2ce99a65e73f309df7523
Tyler Yates <tyates907@gmail.com>
2021-12-29 19:56:40 -0800

    finish quest and questpage

--- src/components/Quest.jsx
+++ src/components/Quest.jsx
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Quest() {
-  return <div></div>;
+export default function Quest(props) {
+  const quest = props.quest;
+  const [open, setOpen] = useState(false);
+
+  const openQuest = () => {
+    setOpen((prevState) => !prevState);
+  };
+  return (
+    <div className="Quest" id={"quest" + quest.id} onClick={openQuest}>
+      {open && quest.imageUrl && (
+        <img className="questImage" src={quest.imageUrl} alt={quest.altText} />
+      )}
+      <section>
+        <h2>{quest.questName}</h2>
+        {open && <h3>{quest.gameTitle}</h3>}
+      </section>
+      <section>
+        {open && <h3>Items Needed:</h3>}
+        {open && <h3>Notes</h3>}
+      </section>
+    </div>
+  );
 }
--- src/screens/QuestPage.jsx
+++ src/screens/QuestPage.jsx
@@ -1,15 +1,62 @@
 import React from "react";
-import Quest from "../components/Item";
+import Quest from "../components/Quest";
 import Navbar from "../components/Navbar";
 
 export default function QuestPage() {
-  const testQuests = [];
+  const testQuests = [
+    {
+      id: 1,
+      questName: "Quest Name",
+      gameTitle: "Game Title",
+      imageUrl: "/testNails.jpeg",
+      altText: "testNails",
+    },
+    {
+      id: 2,
+      questName: "Quest Name 2",
+      gameTitle: "Game Title 2",
+      imageUrl: "/testPlank.jpeg",
+      altText: "testPlank",
+      total: 20,
+    },
+    {
+      id: 3,
+      questName: "Quest Name 3",
+      gameTitle: "Game Title 3",
+      imageUrl: "/testStrawberry.jpeg",
+      altText: "testStrawberry",
+      total: 30,
+    },
+    {
+      id: 4,
+      questName: "Quest Name 4",
+      gameTitle: "Game Title 4",
+      imageUrl: "/testNails.jpeg",
+      altText: "testNails",
+      total: 40,
+    },
+    {
+      id: 5,
+      questName: "Quest Name 5",
+      gameTitle: "Game Title 5",
+      imageUrl: "/testPlank.jpeg",
+      altText: "testPlank",
+      total: 50,
+    },
+    {
+      id: 6,
+      questName: "Quest Name 6",
+      gameTitle: "Game Title 6",
+      imageUrl: "/testStrawberry.jpeg",
+      altText: "testStrawberry",
+      total: 60,
+    },
+  ];
   return (
     <div className="QuestPage">
       <Navbar />
-      <h1>Quests will go here</h1>
-      {testQuests.map((testQuest) => (
-        <Quest quest={testQuest} key={testQuest.id} />
+      {testQuests.map((quest) => (
+        <Quest quest={quest} key={quest.id} />
       ))}
     </div>
   );
