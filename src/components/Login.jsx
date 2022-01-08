565b8765a96bbadc6d5223a5b6deabd7c3d934ba
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-01-08 11:53:55 -0800

    Login Component useState
    username and password are in a state and updated on change

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Login() {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const login = (event) => {
+        event.preventDefault();
+        console.log("Login");
+    }
+
     return (
         <div>
-            <form>
-                <label for = "username">Username</label>
-                <input type = "text" id = "username" placeholder = "Username..."/>
+            <form onSubmit = {login}>
+                <label htmlFor = "username">Username</label>
+                <input type = "text" id = "username" value = {username} onChange = {(element) => setUsername(element.target.value)} placeholder = "Username..."/>
 
-                <label for = "password">Password</label>
-                <input type = "password" id = "password" placeholder = "Password..."/>
+                <label htmlFor = "password">Password</label>
+                <input type = "password" id = "password" value = {password} onChange = {(element) => setPassword(element.target.value)} placeholder = "Password..."/>
 
                 <input type = "submit" value = "Submit"/>
             </form>
