a455018d985c9b7f3b55b31628c1274052b0fc99
Tyler Yates <tyates907@gmail.com>
2022-01-10 18:05:24 -0800

    useNavigate on register input

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-
+    let navigate = useNavigate();
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
@@ -10,17 +10,32 @@ export default function Login() {
         event.preventDefault();
         // Call Login Function
         console.log("Login");
-    }
+    };
 
     return (
-        <form onSubmit = {login} className = "Login">
-            <input type = "text" value = {username} onChange = {(element) => setUsername(element.target.value)} placeholder = "Username" autoFocus/>
-            <input type = "password" value = {password} onChange = {(element) => setPassword(element.target.value)} placeholder = "Password" autoFocus/>
+        <form onSubmit={login} className="Login">
+            <input
+                type="text"
+                value={username}
+                onChange={(element) => setUsername(element.target.value)}
+                placeholder="Username"
+                autoFocus
+            />
+            <input
+                type="password"
+                value={password}
+                onChange={(element) => setPassword(element.target.value)}
+                placeholder="Password"
+                autoFocus
+            />
 
-            <input className="LoginButton" type = "submit" value = "Login"/>
-            <Link to = "/questdex">
-                <input className="LoginButton" type = "button" value = "Register"/>
-            </Link>
+            <input className="LoginButton" type="submit" value="Login" />
+            <input
+                className="LoginButton"
+                type="button"
+                value="Register"
+                onClick={() => navigate("/questdex")}
+            />
         </form>
     );
 }
