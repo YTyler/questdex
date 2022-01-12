9987195ed57f000a1f09a5248a7343cfb6007817
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-01-12 11:28:30 -0800

    Update Register.jsx
    validation for email, password and confirm password

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 
 export default function Register() {
+    // Input field states
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
 
-    const [validUsername, setValidUsername] = useState();
+    // Validation states for each input
+    const [validUsername, setValidUsername] = useState(false);
+    const [validEmail, setValidEmail] = useState(false);
+    const [validPassword, setValidPassword] = useState(false);
+    const [passwordsMatch, setPasswordsMatch] = useState(false);
 
+    // Sets state of username and checks if username is sufficient
     const usernameHandler = (input) => {
         setUsername(input);
         if (input.includes(" ")) {
@@ -19,6 +25,30 @@ export default function Register() {
                 setValidUsername(false);
             }
         }
+    };
+
+    // Sets state of email and checks if email is sufficient
+    const emailHandler = (input) => {
+        setEmail(input);
+        if((input.includes("@")) && (!input.includes(" ")) && (input.length > 2)) {
+            setValidEmail(true);
+        } else {setValidEmail(false);}
+    };
+
+    // Sets state of password and checks if password is sufficient
+    const passwordHandler = (input) => {
+        setPassword(input);
+        if(input.length >= 8 && !input.includes(" ")) {
+            setValidPassword(true);
+        } else {setValidPassword(false);}
+    };
+    
+    // Sets state of confirmation password and checks if password and confirm password match
+    const confirmPasswordHandler = (input) => {
+        setConfirmPassword(input);
+        if(input === password) {
+            setPasswordsMatch(true);
+        } else {setPasswordsMatch(false);}
     };
 
     const checkRegistration = (event) => {
@@ -42,30 +72,30 @@ export default function Register() {
             <input
                 type="email"
                 value={email}
-                onChange={(elem) => setEmail(elem.target.value)}
+                onChange={(elem) => emailHandler(elem.target.value)}
                 placeholder="Email"
             />
             <input
                 type="password"
                 value={password}
-                onChange={(elem) => setPassword(elem.target.value)}
+                onChange={(elem) => passwordHandler(elem.target.value)}
                 placeholder="Password"
             />
             <input
                 type="password"
                 value={confirmPassword}
-                onChange={(elem) => setConfirmPassword(elem.target.value)}
+                onChange={(elem) => confirmPasswordHandler(elem.target.value)}
                 placeholder="Confirm Password"
             />
 
             {/* Register information below */}
             <p className={validUsername ? "Valid" : "Invalid"}>
                 Username must be at least 4 characters and less than 15
-                characters long
+                characters long, no spaces
             </p>
-            <p>Email must be valid</p>
-            <p>Password must be at least 8 characters long</p>
-            <p>Both the Password and the Confirmation Password must match</p>
+            <p className={validEmail ? "Valid" : "Invalid"}>Email must be valid, no spaces</p>
+            <p className={validPassword ? "Valid" : "Invalid"}>Password must be at least 8 characters long, no spaces</p>
+            <p className={passwordsMatch ? "Valid" : "Invalid"}>Both the Password and the Confirmation Password must match</p>
 
             <input
                 type="submit"
