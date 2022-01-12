04a3fc0a83f2947880725afcdb48dd825d9d66a9
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-01-11 18:26:01 -0800

    Basic Validation for Username
    username is still one keystroke short when it comes to changing the color of the validation text

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -7,6 +7,24 @@ export default function Register() {
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
 
+    const [validUsername, setValidUsername] = useState();
+
+    const usernameHandler = (element) => {
+        setUsername(element.target.value);
+        if(username.includes(" ")) {
+            setValidUsername(false);
+            console.log(validUsername);
+        } else {
+            if(username.length >= 4 && username.length < 15) {
+                setValidUsername(true);
+                console.log(validUsername);
+            } else {
+                setValidUsername(false);
+                console.log(validUsername);
+            }
+        }
+    }
+
     const checkRegistration = (event) => {
         event.preventDefault();
         console.log("Register Attempt")
@@ -19,10 +37,17 @@ export default function Register() {
 
     return (
         <form onSubmit={checkRegistration} className="LoginRegister">
-            <input type = "text" value = {username} onChange={(elem) => setUsername(elem.target.value)} placeholder="Username"/>
+            <input type = "text" value = {username} onChange={usernameHandler} placeholder="Username"/>
             <input type = "email" value = {email} onChange={(elem) => setEmail(elem.target.value)} placeholder="Email"/>
             <input type = "password" value = {password} onChange={(elem) => setPassword(elem.target.value)} placeholder="Password"/>
             <input type = "password" value={confirmPassword} onChange={(elem) => setConfirmPassword(elem.target.value)} placeholder="Confirm Password"/>
+
+            {/* Register information below */}
+            <p className = {validUsername ? "Valid" : "Invalid"}>Username must be at least 4 characters and less than 15 characters long</p>
+            <p>Email must be valid</p>
+            <p>Password must be at least 8 characters long</p>
+            <p>Both the Password and the Confirmation Password must match</p>
+
             <input type = "submit" className="LoginRegisterButton" value="Register" />
         </form>
     );
