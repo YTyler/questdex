a2acaffe728d49f8e7380954b2ce53e6b9cfddc8
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-01-16 01:26:19 -0800

    Update HomePage.jsx
    conditionally render items and quests, each function closes the other to account for overlap

--- src/screens/HomePage.jsx
+++ src/screens/HomePage.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { useState } from "react/cjs/react.development";
 import Navbar from "../components/Navbar";
+import ItemPage from "./ItemPage";
+import QuestPage from "./QuestPage";
 
 export default function HomePage(props) {
+
+  const [itemsOpen, setItemsOpen] = useState(false);
+  const [questsOpen, setQuestsOpen] = useState(false);
+
+  const openQuest = () => {
+    setItemsOpen(false);
+    setQuestsOpen(true);
+  }
+
+  const openItem = () => {
+    setQuestsOpen(false);
+    setItemsOpen(true);
+  }
+
   return (
     <>
-      <Navbar status = {props.logging} />
+      <Navbar status = {props.logging} item = {openItem} quest = {openQuest}/>
+      {questsOpen && <QuestPage />}
+      {itemsOpen && <ItemPage />}
     </>
   );
 }
