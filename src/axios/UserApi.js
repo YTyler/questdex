ca303af24f2890f03a1328e36eab5603f2b0de17
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-01-19 12:02:58 -0800

    Update UserApi.js
    corrected login to use params and logout to use pathvariables

--- src/axios/UserApi.js
+++ src/axios/UserApi.js
@@ -1,11 +1,12 @@
 import axios from "../../axiosConfig";
 
+// Backend Takes Request Params
 export async function login(username, password) {
     try {
-        const response = await axios.post('/user', {
-            username: username,
-            password: password
-        });
+        const response = await axios.post('/user', {}, { params: {
+            username,
+            password
+        }});
         return response.data;
     } catch (err) {
         return err;
@@ -13,11 +14,10 @@ export async function login(username, password) {
 }
 
 //! Username's should be unique, may need to change it to take tokens instead
+// Empty request body because it only takes path params
 export async function logout(username) {
     try {
-        const response = await axios.post('/user', {
-            username: username
-        });
+        const response = await axios.post(`/user/${username}`, {});
         return response.data;
     } catch (err) {
         return err;
