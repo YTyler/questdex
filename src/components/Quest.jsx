0831b048806720478719926eac1aecf4844d2e6f
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-01-23 21:20:39 -0800

    Styling and Prop Names
    prop names were changed to fit what is being passed in through the database, added an edit and delete button to each quest

--- src/components/Quest.jsx
+++ src/components/Quest.jsx
@@ -7,19 +7,32 @@ export default function Quest(props) {
   const openQuest = () => {
     setOpen((prevState) => !prevState);
   };
+
+  const deleteQuest = () => {
+    // Delete Quest from database and get it again?
+    console.log("Delete Quest");
+  };
+
+  const editQuest = () => {
+    // I guess just edit the quest name
+    console.log("Edit quest");
+  }
+
   return (
-    <div className="Quest" id={"quest" + quest.id} onClick={openQuest}>
+    <div className="Quest" id={"quest" + quest.quest_id} onClick={openQuest}>
       {open && quest.imageUrl && (
         <img className="questImage" src={quest.imageUrl} alt={quest.altText} />
       )}
       <section>
-        <h2>{quest.questName}</h2>
+        <h2>{quest.quest_name}</h2>
         {open && <h3>{quest.gameTitle}</h3>}
       </section>
       <section>
         {open && <h3>Items Needed:</h3>}
         {open && <h3>Notes</h3>}
       </section>
+      <h3 className = "QuestEdit" onClick={editQuest}>Edit</h3>
+      <h3 className = "QuestDelete" onClick={deleteQuest}>Delete</h3>
     </div>
   );
 }
