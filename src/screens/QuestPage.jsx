d7103c0e32f9ac4012e38626c48065dc56c791dc
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-01-24 00:14:13 -0800

    Editing Quest Implemented
    conditionally render a form for submitting a put request, will likely need to be styled, validation on the questname such that it is not empty

--- src/screens/QuestPage.jsx
+++ src/screens/QuestPage.jsx
@@ -3,61 +3,25 @@ import Quest from "../components/Quest";
 import axios from "../axios/axiosConfig";
 
 export default function QuestPage() {  
-  // const testQuests = [
-    //   {
-      //     id: 1,
-      //     questName: "Quest Name",
-      //     gameTitle: "Game Title",
-      //     imageUrl: "/testNails.jpeg",
-      //     altText: "testNails",
-      //   },
-      //   {
-        //     id: 2,
-        //     questName: "Quest Name 2",
-        //     gameTitle: "Game Title 2",
-        //     imageUrl: "/testPlank.jpeg",
-        //     altText: "testPlank",
-        //     total: 20,
-        //   },
-        //   {
-          //     id: 3,
-  //     questName: "Quest Name 3",
-  //     gameTitle: "Game Title 3",
-  //     imageUrl: "/testStrawberry.jpeg",
-  //     altText: "testStrawberry",
-  //     total: 30,
-  //   },
-  //   {
-    //     id: 4,
-    //     questName: "Quest Name 4",
-    //     gameTitle: "Game Title 4",
-    //     imageUrl: "/testNails.jpeg",
-    //     altText: "testNails",
-    //     total: 40,
-    //   },
-    //   {
-      //     id: 5,
-      //     questName: "Quest Name 5",
-      //     gameTitle: "Game Title 5",
-      //     imageUrl: "/testPlank.jpeg",
-      //     altText: "testPlank",
-      //     total: 50,
-      //   },
-      //   {
-        //     id: 6,
-        //     questName: "Quest Name 6",
-        //     gameTitle: "Game Title 6",
-        //     imageUrl: "/testStrawberry.jpeg",
-        //     altText: "testStrawberry",
-        //     total: 60,
-        //   },
-        // ];
 
   const [quests, setQuests] = useState([]);
 
-  const deleteHandler = (obj) => {
-    // Axios function for deleting a quest
-    axios.delete(`/quests/${obj.quest_id}`);
+  // Axios function for deleting a quest
+  const deleteHandler = (quest) => {
+    axios.delete(`/quests/${quest.quest_id}`);
+  }
+
+  const editHandler = (quest) => {
+    try {
+      axios.put(`/quests/${quest.quest_id}`, {
+        quest_id: quest.quest_id, 
+        game_id: quest.game_id, 
+        user_id: quest.user_id, 
+        quest_name: quest.quest_name
+      });
+    } catch (err) {
+      console.log(err);
+    }
   }
         
   // Axios function for getting all quests
@@ -72,7 +36,7 @@ export default function QuestPage() {
   return (
     <div className="QuestPage">
       {quests.map((quest) => (
-        <Quest quest={quest} key={quest.quest_id} delete = {deleteHandler} />
+        <Quest quest={quest} key={quest.quest_id} delete = {deleteHandler} edit = {editHandler} />
       ))}
     </div>
   );
