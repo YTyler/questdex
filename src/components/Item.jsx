6f1db90f2fdc2eb42b0df63ce437a115eed62a49
Tyler Yates <tyates907@gmail.com>
2022-01-25 17:51:14 -0800

    item CRUD basics

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,26 +1,68 @@
 import React, { useState } from "react";
 
 export default function Item(props) {
-  const item = props.item;
-  const [open, setOpen] = useState(false);
+    const item = props.item;
+    const [open, setOpen] = useState(false);
+    const [openEdit, setOpenEdit] = useState(false);
+    const [itemName, setItemName] = useState(item.item_name);
+    const [validName, setValidName] = useState(false);
 
-  const openItem = () => {
-    setOpen((prevState) => !prevState);
-  };
-  return (
-    <div className="Item" id={"item" + item.id} onClick={openItem}>
-      <section>
-        {open && item.imageUrl && (
-          <img className="itemImage" src={item.imageUrl} alt={item.altText} />
-        )}
-        <h2>{item.itemName}</h2>
-        {open && <h3>{item.gameTitle}</h3>}
-      </section>
-      <section>
-        <h2>Number Needed: {item.total}</h2>
-        {open && <h3>Related Quests:</h3>}
-        {open && <h3>Notes</h3>}
-      </section>
-    </div>
-  );
+    const openItem = () => {
+        setOpen((prevState) => !prevState);
+    };
+
+    const editItemName = (input) => {
+        setItemName(input);
+        if (input.trim().length > 0) {
+            setValidName(true);
+        } else {
+            setValidName(false);
+        }
+    };
+
+    const submitEdit = (event) => {
+        event.preventDefault();
+        if (validName) {
+            item.item_name = itemName;
+            props.edit(props.item);
+            setOpenEdit(false);
+        }
+    };
+
+    return (
+        <div className="Item" id={"item" + item.id} onClick={openItem}>
+            {open && item.imageUrl && (
+                <img
+                    className="itemImage"
+                    src={item.imageUrl}
+                    alt={item.altText}
+                />
+            )}
+            <section>
+                {openEdit ? (
+                    <form onSubmit={(event) => submitEdit(event)}>
+                        <input
+                            type="text"
+                            value={itemName}
+                            onChange={(elem) => editItemName(elem.target.value)}
+                        />
+                    </form>
+                ) : (
+                    <h2>{item.item_name}</h2>
+                )}
+                {open && <h3>{item.gameTitle}</h3>}
+            </section>
+            <section>
+                <h2>Number Needed: {item.total}</h2>
+                {open && <h3>Related Quests:</h3>}
+                {open && <h3>Notes</h3>}
+            </section>
+            <h3 className="QuestEdit" onClick={() => setOpenEdit(true)}>
+                Edit
+            </h3>
+            <h3 className="QuestDelete" onClick={() => props.delete(item)}>
+                Delete
+            </h3>
+        </div>
+    );
 }
--- src/screens/ItemPage.jsx
+++ src/screens/ItemPage.jsx
@@ -1,63 +1,101 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Item from "../components/Item";
+import axios from "../axios/axiosConfig";
 
 export default function ItemPage() {
-  const testItems = [
-    {
-      id: 1,
-      itemName: "Item Name",
-      gameTitle: "Game Title",
-      imageUrl: "/testNails.jpeg",
-      altText: "testNails",
-      total: 10,
-    },
-    {
-      id: 2,
-      itemName: "Item Name 2",
-      gameTitle: "Game Title 2",
-      imageUrl: "/testPlank.jpeg",
-      altText: "testPlank",
-      total: 20,
-    },
-    {
-      id: 3,
-      itemName: "Item Name 3",
-      gameTitle: "Game Title 3",
-      imageUrl: "/testStrawberry.jpeg",
-      altText: "testStrawberry",
-      total: 30,
-    },
-    {
-      id: 4,
-      itemName: "Item Name 4",
-      gameTitle: "Game Title 4",
-      imageUrl: "/testNails.jpeg",
-      altText: "testNails",
-      total: 40,
-    },
-    {
-      id: 5,
-      itemName: "Item Name 5",
-      gameTitle: "Game Title 5",
-      imageUrl: "/testPlank.jpeg",
-      altText: "testPlank",
-      total: 50,
-    },
-    {
-      id: 6,
-      itemName: "Item Name 6",
-      gameTitle: "Game Title 6",
-      imageUrl: "/testStrawberry.jpeg",
-      altText: "testStrawberry",
-      total: 60,
-    },
-  ];
-  
-  return (
-    <div className="ItemPage">
-      {testItems.map((testItem) => (
-        <Item item={testItem} key={testItem.id} />
-      ))}
-    </div>
-  );
+    const [items, setItems] = useState([]);
+    const [addItem, setAddItem] = useState(false);
+    const [itemName, setItemName] = useState("");
+    const [validItem, setValidItem] = useState();
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        try {
+            axios.get("/quests").then((res) => setItems(res.data));
+        } catch (err) {
+            console.log(err);
+        }
+    }, [isLoading]);
+
+    const itemNameHandler = (input) => {
+        setItemName(input);
+        if (input.trim().length > 0) {
+            setValidItem(true);
+        } else {
+            setValidItem(false);
+        }
+    };
+
+    const addItemHandler = async () => {
+        if (validItem) {
+            try {
+                //! User Id needs to be taken from a logged in user
+                //! Will need to be updated
+                await axios.post("/items", {
+                    game_id: 1,
+                    item_name: itemName,
+                    quantity_needed: 0,
+                });
+            } catch (err) {
+                console.log(err);
+            } finally {
+                setItemName("");
+                setIsLoading((prev) => !prev);
+            }
+        }
+    };
+
+    // Axios function for deleting a quest
+    const deleteHandler = async (item) => {
+        await axios.delete(`/items/${item.item_id}`);
+        setIsLoading((prev) => !prev);
+    };
+
+    // Axios function for editing a quest
+    const editHandler = async (item) => {
+        try {
+            await axios.put(`/items/${item.item_id}`, {
+                item_id: item.item_id,
+                game_id: item.game_id,
+                item_name: itemName,
+                quantity_needed: item.quantity_needed,
+            });
+            setIsLoading((prev) => !prev);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    return (
+        <div className="ItemPage">
+            {addItem ? (
+                <div>
+                    <input
+                        type="text"
+                        value={itemName}
+                        onChange={(elem) => itemNameHandler(elem.target.value)}
+                        placeholder="Itemname..."
+                    />
+                    <input type="submit" onClick={addItemHandler} />
+                    <input
+                        type="button"
+                        value="Cancel"
+                        onClick={() => setAddItem(false)}
+                    />
+                </div>
+            ) : (
+                <div className="AddItem">
+                    <button onClick={() => setAddItem(true)}>Add Item</button>
+                </div>
+            )}
+            {items.map((item) => (
+                <Item
+                    item={item}
+                    key={item.id}
+                    edit={editHandler}
+                    delete={deleteHandler}
+                />
+            ))}
+        </div>
+    );
 }
