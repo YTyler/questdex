2b2b8fe00044472533022ba284c6cc846f3dd8bb
Tyler Yates <tyates907@gmail.com>
2022-01-25 19:17:49 -0800

    format

--- src/screens/LoginPage.jsx
+++ src/screens/LoginPage.jsx
@@ -3,19 +3,22 @@ import Login from "../components/Login";
 import Register from "../components/Register";
 
 export default function LoginPage(props) {
-
     const [isLoggingIn, setIsLoggingIn] = useState(true);
 
     const registerHandler = (element) => {
         setIsLoggingIn(element);
-    }
+    };
 
     return (
         <>
-            <div className = "Navbar">
+            <div className="Navbar">
                 <h1>{isLoggingIn ? "Login" : "Register"}</h1>
             </div>
-            {isLoggingIn ? <Login log = {props.logging} registerHandler = {registerHandler} /> : <Register log = {props.logging}/>}
+            {isLoggingIn ? (
+                <Login log={props.logging} registerHandler={registerHandler} />
+            ) : (
+                <Register log={props.logging} />
+            )}
         </>
     );
 }
