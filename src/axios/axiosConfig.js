5b0a406e9f090c505cf766e8b99c306354c84d0d
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-01-26 08:27:33 -0800

    Update axiosConfig.js
    axios config url had an extra slash and space causing it to error

--- src/axios/axiosConfig.js
+++ src/axios/axiosConfig.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://questdex-env.eba-7xcqqbdr.us-east-2.elasticbeanstalk.com/ ',
+    baseURL: 'http://questdex-env.eba-7xcqqbdr.us-east-2.elasticbeanstalk.com',
 });
